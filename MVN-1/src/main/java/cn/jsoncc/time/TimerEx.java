9277c0fd6bcf2eca85e348a4ada94cb6fcb19424
JsonCC <896415482@qq.com>
2023-04-25 17:06:07 +0800

    定时

--- MVN-1/src/main/java/cn/jsoncc/time/TimerEx.java
+++ MVN-1/src/main/java/cn/jsoncc/time/TimerEx.java
@@ -7,20 +7,27 @@ import java.util.concurrent.Executors;
         import java.util.concurrent.TimeUnit;
 
 public class TimerEx {
-    private final static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    private final static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) {
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
         Runnable task = () -> {
-            //输出的内容
-            System.out.println(format.format(new Date()));
+            try {
+                //输出的内容
+                System.out.println(format.format(new Date()));
+                //间隔的时间，单位：毫秒，1000毫秒=1秒
+//                Thread.sleep(3000);
+            } catch (Exception e) {
+                System.out.println("定时设置错误");
+            }
+
         };
 
         //成功运行后在多长时间之后开始输出，单位：秒
         int initialDelay = 0;
         //输出的间隔，单位：秒
-        int period = 60;
+        int period = 1;
 
         executor.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS);
     }
