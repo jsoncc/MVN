fd0d74a5e98d1b0af05224fa24902553d61221d4
JsonCC <896415482@qq.com>
2023-04-26 16:38:17 +0800

    控制台输入

--- MVN-1/src/main/java/cn/jsoncc/consolescan/ComputeAreaWithConsolelnput.java
+++ MVN-1/src/main/java/cn/jsoncc/consolescan/ComputeAreaWithConsolelnput.java
@@ -1,7 +1,13 @@
 package cn.jsoncc.consolescan;
 
+import java.util.Scanner;
+
 public class ComputeAreaWithConsolelnput {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入半径：");
+        double r = sc.nextDouble();
+        System.out.println("圆的面积为：" + 3.14 * r * r);
+
     }
 }
