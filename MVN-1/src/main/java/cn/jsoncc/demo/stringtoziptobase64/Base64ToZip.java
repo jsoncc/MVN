7ba71aa6ab29a1e9bf8331ba83b2ab8e539ed289
JsonCC <896415482@qq.com>
2023-09-07 10:32:46 +0800

    string类型转为压缩包，另外再转成base64码

---
+++ MVN-1/src/main/java/cn/jsoncc/demo/stringtoziptobase64/Base64ToZip.java
@@ -0,0 +1,31 @@
+package cn.jsoncc.demo.stringtoziptobase64;
+
+import java.util.Base64;
+import java.io.*;
+
+/**
+ * @author JsonCC
+ * @date 2023/9/1 15:57
+ * @desc 将一个Base64码转成压缩包,保存到本地指定路径
+ */
+
+public class Base64ToZip {
+    public static void main(String[] args) {
+        String base64EncodedData = "fp.bwjf.cn/downInvoice/026/050003521107_99915455?sign=bac20f3574883f6f9bb1389f62941188bbed044a"; // 替换为你的Base64编码字符串
+        String outputFilePath = "D:\\JavaFiles\\IdeaProjects\\output.zip"; // 输出文件路径
+
+        try {
+            // 解码Base64字符串并写入文件
+            byte[] decodedBytes = Base64.getDecoder().decode(base64EncodedData);
+            FileOutputStream outputStream = new FileOutputStream(outputFilePath);
+            outputStream.write(decodedBytes);
+            outputStream.close();
+
+            System.out.println("文件解码成功并保存到 " + outputFilePath);
+
+            // 现在可以打开或解压 output.zip 文件来访问压缩包内容
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
