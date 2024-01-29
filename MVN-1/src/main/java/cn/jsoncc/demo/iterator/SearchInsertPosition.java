5c1ff1416a651585259e7d53335aea2b22f601fd
JsonCC <896415482@qq.com>
2024-01-29 13:32:00 +0800

    算法题-二分查找法

--- MVN-1/src/main/java/cn/jsoncc/demo/iterator/SearchInsertPosition.java
+++ MVN-1/src/main/java/cn/jsoncc/demo/iterator/SearchInsertPosition.java
@@ -10,13 +10,19 @@ package cn.jsoncc.demo.iterator;
  * ●[1,3,5,6], 2 → 1
  * ●[1,3,5,6], 7 → 4
  * ●[1,3,5,6], 0 → 0
+ *
+ * 时间复杂度：
+ * 二分法的时间复杂度是 O(log n)，其中 n 是数据集合的大小。在每一次比较后，搜索范围都被缩小为原来的一半，因此它具有非常高效的查找速度。
+ * 空间复杂度：
+ * 二分法的空间复杂度是 O(1)，也就是常量级的空间。它并不需要额外的存储空间，只需几个变量来保存边界和中间索引。
  */
 public class SearchInsertPosition {
     public static int number(int[] array, int n) {
         int front = 0;
         int after = array.length - 1;
         while (front <= after) {
-            int mid = front + (after - front) / 2;
+            int mid = front + (after - front) / 2;// 如果a = 1.6 那么int a = 1;
+            System.out.println("数组中位数：" + array[mid]);
             if (array[mid] == n) {
                 return mid;
             } else if (array[mid] < n) {
@@ -29,11 +35,11 @@ public class SearchInsertPosition {
     }
 
     public static void main(String[] args) {
-        int[] array1 = {1, 3};
-        System.out.println(number(array1, 2)); // 输出: 2
+//        int[] array1 = {1, 3};
+//        System.out.println(number(array1, 2)); // 输出: 2
 
-//        int[] array2 = {1, 3, 5, 6};
-//        System.out.println(number(array2, 2)); // 输出: 1
+        int[] array2 = {1, 3, 5, 6};
+        System.out.println(number(array2, 5)); // 输出: 2
 //
 //        int[] array3 = {1, 3, 5, 6};
 //        System.out.println(number(array3, 7)); // 输出: 4
