8266d39d2cdb18683947d8c6c4c0b314567f7445
JsonCC <chenjiansong111@gmail.com>
2025-03-16 20:10:11 +0800

    feat(TestController): 添加注释测试类

---
+++ MVN-3/src/main/java/cn/jsoncc/controller/TestController.java
@@ -0,0 +1,11 @@
+package cn.jsoncc.controller;
+
+/**
+ * @Author JsonCC
+ * @Date 2025/3/16 20:05
+ * @PackageName:cn.jsoncc.controller
+ * @ClassName: TestController
+ * @Description: TODO
+ */
+public class TestController {
+}
